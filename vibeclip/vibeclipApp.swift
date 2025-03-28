05e3e83a3615fae48adaeed2ffaeefbc7b1e62fc
Allan <allan.miller@outlook.com>
2025-03-27 20:15:13 -0500

    Initial Commit

---
+++ vibeclip/vibeclipApp.swift
@@ -0,0 +1,17 @@
+//
+//  vibeclipApp.swift
+//  vibeclip
+//
+//  Created by Allan Miller on 3/27/25.
+//
+
+import SwiftUI
+
+@main
+struct vibeclipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
